7f7c3c44a703ba1b7d5f043a9e16ab7ad8fb2785
Sahil Meena <sahilmeena2031@gmail.com>
2024-11-19 14:49:57 +0530

    defined the user Schema

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./database/dbConnect.js";
 
 dotenv.config({});
+
+// calling database connection here
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
