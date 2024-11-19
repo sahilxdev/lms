b465cdf1a178cccc726a8689c5f2dfef5688ced1
Sahil Meena <sahilmeena2031@gmail.com>
2024-11-19 15:21:39 +0530

    \\\\\\\\\

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,17 +9,64 @@ export const register = async (req, res) => {
         message: "All fields are required",
       });
     }
-    const user = await User.findOne({email});
-    if (user){
-        return res.status(400).json({
-            success: false,
-            message: "User already exist with this email."
-        })
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exist with this email.",
+      });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     await User.create({
-        name,
-        email,
-        password
-    })
-  } catch (error) {}
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({
+      success: true,
+      message: "Account created succesfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to register",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    const user = await User.findOne({email});
+    if (user) {
+        return res.status(400).json({
+          success: false,
+          message: "Incorrect email or password",
+        });
+      }
+      const isPasswordMatch = await bcrypt.compare(password, user.password)
+      if (!isPasswordMatch) {
+        return res.status(400).json({
+          success: false,
+          message: "Incorrect email or password",
+        });
+      }
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to login",
+    });
+  }
 };
