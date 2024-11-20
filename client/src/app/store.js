7cfb3a0c57001cfb87914790e790d2db0f768b6d
Sahil Meena <sahilmeena2031@gmail.com>
2024-11-20 13:05:34 +0530

    added Redux store for RTK

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
 
 export const appStore = configureStore({
     reducer:{
-        
+        auth: authReducer
     }
 })
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { appStore } from './app/store';
+import { appStore } from "./app/store";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={appStore}>
-    <App />
+      <App />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
