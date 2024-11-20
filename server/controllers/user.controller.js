02e7014acd09366b05a5b6b7f160f3485b831b4d
Sahil Meena <sahilmeena2031@gmail.com>
2024-11-20 12:32:31 +0530

    built authentication and added RTK Query

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,12 @@
 import { User } from "../models/user.model.js";
+import { generateToken } from "../utils/generateToken.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-      res.status(400).json({
+      return res.status(400).json({
         success: false,
         message: "All fields are required",
       });
@@ -41,26 +43,28 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    if (!name || !email || !password) {
+    if (!email || !password) {
       res.status(400).json({
         success: false,
         message: "All fields are required",
       });
     }
-    const user = await User.findOne({email});
-    if (user) {
-        return res.status(400).json({
-          success: false,
-          message: "Incorrect email or password",
-        });
-      }
-      const isPasswordMatch = await bcrypt.compare(password, user.password)
-      if (!isPasswordMatch) {
-        return res.status(400).json({
-          success: false,
-          message: "Incorrect email or password",
-        });
-      }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+    
+    generateToken(res,user,`Welcome back ${user.name}`)
 
   } catch (error) {
     console.log(error);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/dbConnect.js";
+import userRoute from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({});
 
@@ -11,7 +14,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+    credentials: true,
+  })
+);
+
+// apis
+app.use("/api/v1/user", userRoute);
+
+app.get("/home", (_, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Hello i am coming from backend",
+  });
+});
+
 app.listen(PORT, () => {
-    console.log(`Server listen at port ${PORT}`);
-    
+  console.log(`Server listen at port ${PORT}`);
 });
